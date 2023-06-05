1162ce35cb4e3151baff2e2cfa0bdb621919a3e1
Israel <israel.com.ihf80@gmail.com>
2023-06-05 11:36:02 -0300

    Add files via upload

---
+++ src/Cliente.ts
@@ -0,0 +1,70 @@
+import Endereco from "./Endereco";
+import Telefone from "./Telefone";
+
+export default class Cliente{
+    private _nome : String;
+    private _cpf : number;
+    private _dataNascimento : number;
+    private _sexo : String;
+    private _endereco : Endereco;
+    private _telefones : Telefone[];
+
+    constructor(nome: String, cpf: number, dataNascimento: number, sexo: String, endereco: Endereco, telefones: Telefone[]){
+        this._nome = nome;
+        this._cpf = cpf;
+        this._dataNascimento = dataNascimento;
+        this._sexo = sexo;
+        this._endereco = endereco;
+        this._telefones = telefones;
+
+    }
+
+    get nome () : String {
+        return this._nome;
+    }
+
+    set nome (nome: String){
+        this._nome = nome;
+    } 
+
+    get cpf() : number {
+        return this._cpf;
+    }
+
+    set cpf(cpf: number){
+        this._cpf = cpf;
+    } 
+
+    get dataNascimento() : number {
+        return this._dataNascimento;
+    }
+
+    set dataNascimento(dataNascimento: number){
+        this._dataNascimento = this.dataNascimento;
+    }
+
+    get sexo() : String {
+        return this._sexo;
+    }
+
+    set sexo(nome: String){
+        this._nome = nome;
+    } 
+
+    get endereco() : Endereco {
+        return this._endereco;
+    }
+
+    set endereco(endereco: Endereco){
+        this._endereco = endereco;
+    } 
+
+    get telefones() : Telefone[] {
+        return this._telefones;
+    }
+
+    set telefones(telefones: Telefone[]){
+        this._telefones = telefones;
+    } 
+
+}
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Cliente from "./Cliente";
+import Endereco from "./Endereco";
+import Telefone from "./Telefone";
+import Venda from "./Venda";
+import Produto from "./Produto";
+
+let endereco1 : Endereco = new Endereco("Avenida Helvétia", 1104, "Guarapuava", 1);
+let produto1: Produto = new Produto(1, "Camiseta Azul")
+
+
+
+
+
+
+
