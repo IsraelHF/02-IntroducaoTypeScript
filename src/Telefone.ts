cc9f12607f46b0a8edd8b239481649ab95292d84
Israel <israel.com.ihf80@gmail.com>
2023-06-13 22:38:18 -0300

    Update Telefone.ts

--- src/Telefone.ts
+++ src/Telefone.ts
@@ -1,9 +1,14 @@
+enum TELEFONE{
+    Residencial,
+    Pessoal
+}
+
 export default class Telefone{
     private _ddd: String;
     private _numero: number;
-    private _tipo: String;
+    private _tipo: TELEFONE;
 
-    constructor(ddd: String, numero: number, tipo: String){
+    constructor(ddd: String, numero: number, tipo: TELEFONE){
         this._ddd = ddd;
         this._numero = numero;
         this._tipo = tipo;
@@ -25,11 +30,11 @@ export default class Telefone{
         this._numero = numero;
     } 
 
-    get tipo() : String {
+    get tipo() : TELEFONE {
         return this._tipo;
     }
 
-    set tipo(tipo: String){
+    set tipo(tipo: TELEFONE){
         this._tipo = tipo;
     } 
 
